f6094ec37ba788c436ac179e08fe2a098daab97a
Muhhamed Sallam <m.sallam2622000@gmail.com>
2023-05-03 00:28:22 +0300

    created producer/consumer simple example

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const dotenv = require('dotenv');
 const setupDB = require("ep-det-core/startup-modules/db");
 const Patient = require("ep-det-core/models/mongoose/patient");
+const amqp = require('amqplib');
 // const mongoose = require("mongoose");
 
-const queueName = 'my_queue';
-const mqUrl = 'amqp://localhost:5672';
+const mqUrl = 'amqp://localhost';
+const queueName = 'myqueue';
+const rabbitSettings = {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5672,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL'],
+  };
+
+async function produceMessages() {
+    try {
+        const connection = await amqp.connect(rabbitSettings);
+        if(connection) {
+          console.log('Connected to ranbbitMQ successfully');
+        }
+        const channel = await connection.createChannel();
+        const message = 'Hello world';
+        
+        await channel.assertQueue(queueName);
+        await channel.sendToQueue(queueName, Buffer.from(message));
+        console.log(`Message "${message}" sent to queue "${queueName}"`);
+
+        // await channel.close();
+        // await connection.close();
+      } catch (error) {
+        console.error(error);
+      }
+}
 
 async function consumeMessages() {
     try {
         const connection = await amqp.connect(mqUrl);
+        if(connection) {
+          console.log('Connected to ranbbitMQ successfully');
+        }
         const channel = await connection.createChannel();
     
         await channel.assertQueue(queueName);
@@ -24,7 +57,7 @@ async function consumeMessages() {
 
 
 dotenv.config({path:'.env'});
-
 console.log(process.env.MONGO_URI);
 setupDB(console);
-//consumeMessages();
+produceMessages();
+consumeMessages();
