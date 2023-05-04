dee0476977155686e2f90dafdcb5743d9fbb82d7
Omar Alaa <omaralaaabdelkader@gmail.com>
2023-05-04 15:00:49 +0300

    Consume ML-server messages, fixing ack bug

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const setupDB = require("ep-det-core/startup-modules/db");
 const Patient = require("ep-det-core/models/mongoose/patient");
-const amqp = require('amqplib');
-const produceMessages = require('./rabbitMQ test/producer');
-const consumeMessages = require('./rabbitMQ test/consumer');
-const redisService = require('./services/redisService');
-
+const amqp = require("amqplib");
+const { consumeMessages } = require("./rabbitMQ test/consumer");
+const redisService = require("./services/redisService");
 
 //getting some random patient emerngency contact from the database
 //hard coding the patient id for now : 641e1b6ecfd4ffd65226a778
 async function getPatient() {
+  // const patient = await Patient.findById("641e1b6ecfd4ffd65226a778");
+  // const emergencyContact = patient.emergencyContact[0];
+  const emergencyContact = {
+    name: "John Doe",
+    phoneNumber: "12345678900",
+  };
+  const predictionResult = "Epilepsy detected";
 
-// const patient = await Patient.findById("641e1b6ecfd4ffd65226a778");
-// const emergencyContact = patient.emergencyContact[0];
-const emergencyContact = {
-    "name": "John Doe",
-    "phoneNumber": "12345678900"
-}
-const predictionResult = "Epilepsy detected";
-
-  if(emergencyContact){
-      console.log(emergencyContact.name);
+  if (emergencyContact) {
+    console.log(emergencyContact.name);
   }
   redisService.setContact(emergencyContact.phoneNumber, predictionResult);
 
   redisService.getContact(emergencyContact.phoneNumber);
 
   //redisService.deleteContact(emergencyContact.phoneNumber);
-
 }
-   
 
-
-
+dotenv.config(".env");
 
 // setupDB(console);
 getPatient();
 // produceMessages();
-// consumeMessages();
+
+(async () => {
+  await consumeMessages();
+})();
--- rabbitMQ test/consumer.js
+++ rabbitMQ test/consumer.js
@@ -1,21 +1,28 @@
-const amqp = require('amqplib');
-
+const amqp = require("amqplib");
 
 module.exports.consumeMessages = async () => {
-    try {
-        const connection = await amqp.connect(process.env.AMQP_URL);
-        if(connection) {
-          console.log('Connected to ranbbitMQ successfully');
-        }
-        const channel = await connection.createChannel();
-    
-        await channel.assertQueue(process.env.QUEUE_NAME);
-        console.log(`Listening for messages on queue "${process.env.QUEUE_NAME}"...`);
-    
-        channel.consume(process.env.QUEUE_NAME, (message) => {
-          console.log(`Received message: ${message.content.toString()}`);
-        });
-      } catch (error) {
-        console.error(error);
+  try {
+    const connection = await amqp.connect(process.env.AMQP_URL);
+    if (connection) {
+      console.log("Connected to ranbbitMQ successfully");
+    }
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(process.env.QUEUE_NAME);
+    console.log(
+      `Listening for messages on queue "${process.env.QUEUE_NAME}"...`
+    );
+
+    channel.consume(
+      process.env.QUEUE_NAME,
+      (message) => {
+        console.log(`Received message: ${message.content.toString()}`);
+      },
+      {
+        noAck: true,
       }
-}
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
